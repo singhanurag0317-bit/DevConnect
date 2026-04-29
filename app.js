8cebd5a440a76e848dc0118828bf11d5d5ad54ca
Anurag Singh <singhanurag0317@gmail.com>
2026-04-29 09:12:59 +0530

    css updated 3

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(express.static('public'));
 
 // View engine
 app.set('view engine', 'ejs');
